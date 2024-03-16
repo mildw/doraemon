f53d98e32a9db465b6d74a0ffe4903d5ccdf04a3
mildw <mildw428@gmail.com>
2024-03-16 20:57:05 +0900

    스케쥴 타입 추가

--- scheduler/src/main/java/com/example/scheduler/task/Schedule.java
+++ scheduler/src/main/java/com/example/scheduler/task/Schedule.java
@@ -7,8 +7,9 @@ public class Schedule {
   
     public static final long FIXED_DELAY_1S = SECOND;  
   
-    public static final long FIXED_DELAY_1M = 60 * FIXED_DELAY_1S;  
-  
-    //초 (0-59) 분 (0-59) 시 (0-23) 일 (1-31) 월 (1-12 또는 JAN-DEC) 요일 (0-7 또는 SUN-SAT)    public static final String EVERY_12AM = "0 0 0 * * *";  
-  
+    public static final long FIXED_DELAY_1M = 60 * FIXED_DELAY_1S;
+
+    //초 (0-59) 분 (0-59) 시 (0-23) 일 (1-31) 월 (1-12 또는 JAN-DEC) 요일 (0-7 또는 SUN-SAT)
+    public static final String EVERY_12AM = "0 0 0 * * *";
+
 }
